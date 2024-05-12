d94a5039363d473ecc1c2e84f2940e6e57c8eda8
Martin Baňas <martinbanas19@gmail.com>
2024-05-13 00:04:25 +0200

    Added time measurements to the test_run.m

--- matlab/run_test.m
+++ matlab/run_test.m
@@ -6,10 +6,14 @@ addpath(genpath('Functions'));
 addpath(genpath('Test'));
 
 model = load_system('ParentTest');
+t1 = datetime;
 folderPath = "configs";
 parser = JsonUseCasesParser(folderPath);
 [useCases, inlining, maxParentDepth] = parser.ParseConfigs();
 config = ConfigFileRun(useCases, inlining, model, maxParentDepth);
 config.evalUseCasesOnModel();
 JsonUseCasesOutputPrinter.printToFile(config.Output, "test.json");
+t2 = datetime;
+dif = milliseconds(t2-t1);
+fprintf('Time: %0.6e', dif);
 end
